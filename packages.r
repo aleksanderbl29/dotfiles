9106205042ea81805bdc89eba3552aabc8f8ac8b
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2023-12-20 18:27:31 +0100

    add gapminder and patchwork and format list

--- packages.r
+++ packages.r
@@ -1,3 +1,14 @@
+## General
 install.packages("tidyverse")
+
+## Data import
 install.packages("read_excel")
+
+## Data cleaning
 install.packages("janitor")
+
+## Graphing
+install.packages("patchwork")
+
+## Data
+install.packages("gapminder")
